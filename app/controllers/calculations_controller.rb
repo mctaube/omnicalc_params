54cf15fc8c237e052e03588d109d06a4cc466137
Matt Taube <mctaube@users.noreply.github.com>
2017-04-22 21:11:10 -0500

    Up to payment form

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -5,15 +5,8 @@ class CalculationsController < ApplicationController
     render ("calculations/square.html.erb")
   end
 
-
   def square_form
     render("calculations/square_form.html.erb")
-  end
-
-  def flex_square_root
-    @user_number= params["num"].to_f
-    @squareroot = @user_number **(0.5)
-    render("calculations/flex_square_root.html.erb")
   end
 
   def flex_square_5
@@ -22,4 +15,43 @@ class CalculationsController < ApplicationController
 
     render("calculations/flex_square_5.html.erb")
   end
+
+  # square root
+  def flex_square_root
+    @user_number= params["num"].to_f
+    @squareroot = @user_number **(0.5)
+    render("calculations/flex_square_root.html.erb")
+  end
+
+  def square_root_form
+    render("calculations/square_root_form.html.erb")
+  end
+
+  def square_root
+    @user_number = params[:user_number].to_f
+    @squareroot = @user_number **(0.5)
+    render ("calculations/square_root.html.erb")
+  end
+
+  # loan payment calcs
+  def flex_payment
+    @interest = (params["basis_points"].to_f)/100
+    @years = params["number_of_years"].to_f
+    @principal = params["present_value"].to_f
+    @payments =((@interest/100/12)*@principal)/(1-((1+@interest/100/12))**(-@years*12))
+    render("calculations/flex_payment.html.erb")
+  end
+
+  def payment_form
+    render("calculations/payment_form.html.erb")
+  end
+
+  def payment
+    @interest = (params[:interest_rate].to_f)
+    @years = params[:years].to_f
+    @principal = params[:principal].to_f
+    @payments =((@interest/100/12)*@principal)/(1-((1+@interest/100/12))**(-@years*12))
+    render ("calculations/payment.html.erb")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ Rails.application.routes.draw do
   get("/square/new", { :controller => "calculations", :action => "square_form"})
   get("/square/results", { :controller => "calculations", :action => "square"})
 
-# square root calculations
+  # square root calculations
   get("/flexible/square_root/:num", { :controller => "calculations", :action => "flex_square_root"})
+  get("/square_root/new", { :controller => "calculations", :action => "square_root_form"})
+  get("/square_root/results", { :controller => "calculations", :action => "square_root"})
 
+  # flexible payments
+  get("/flexible/payment/:basis_points/:number_of_years/:present_value", { :controller => "calculations", :action => "flex_payment"})
+  get("/payment/new", { :controller => "calculations", :action => "payment_form"})
+  get("/payment/results", { :controller => "calculations", :action => "payment"})
 
+  
 end
